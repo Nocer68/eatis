d062b3cac23a3e6f6b6c7d88459d43e9d995ec3d
Valentin Figueiredo <valentinfigueiredo@protonmail.com>
2020-11-25 16:36:28 +0100

    navbarscroll

--- public/app.js
+++ public/app.js
@@ -2,16 +2,18 @@ window.onscroll = function () {
 
   if (document.documentElement.scrollTop > 490) {
     document.getElementById("navbar").style.background = "white";
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.color = "black";
+    document.getElementById("navbar").style.padding = "30px 50px";
+    document.getElementById("logo").style.color = "white";
     document.getElementById("navbar-right").style.color = "black";
+
   }
   else {
     document.getElementById("navbar").style.background = "transparent";
-    document.getElementById("navbar").style.padding = "90px 10px";
+    document.getElementById("navbar").style.padding = "50px 10px";
     document.getElementById("logo").style.color = "black";
     document.getElementById("navbar-right").style.color = "black";
   }
 
 
 }
+
