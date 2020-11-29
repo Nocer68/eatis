3e3ccac53501dceaba40aa039924756f5eb06168
Valentin Figueiredo <valentinfigueiredo@protonmail.com>
2020-11-29 19:56:03 +0100

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'eatis-blue': '#1f285c',
+      }
+    },
   },
   variants: {
     extend: {},
