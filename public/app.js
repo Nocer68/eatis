e4faf3c75069017b93fac3410269bc854b19b416
Valentin Figueiredo <valentinfigueiredo@protonmail.com>
2020-12-04 14:28:53 +0100

    navbar_ok

--- public/app.js
+++ public/app.js
@@ -3,15 +3,13 @@ window.onscroll = function () {
   if (document.documentElement.scrollTop > 490) {
     document.getElementById("navbar").style.background = "white";
     document.getElementById("navbar").style.padding = "30px 50px";
-    document.getElementById("logo").style.color = "white";
-    document.getElementById("navbar-right").style.color = "black";
+    document.getElementById("navbar").style.boxShadow = "0 0 5px #999999";
 
   }
   else {
     document.getElementById("navbar").style.background = "transparent";
     document.getElementById("navbar").style.padding = "50px 10px";
-    document.getElementById("logo").style.color = "black";
-    document.getElementById("navbar-right").style.color = "black";
+    document.getElementById("navbar").style.boxShadow = "0 0 0px";
   }
 
 
