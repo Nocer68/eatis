8cb9d6a16a0940fe1bcce2e0db15c3a6d98677a9
Valentin Figueiredo <valentinfigueiredo@protonmail.com>
2020-12-06 18:22:59 +0100

    navbarok

--- public/app.js
+++ public/app.js
@@ -1,17 +1,5 @@
-window.onscroll = function () {
-
-  if (document.documentElement.scrollTop > 490) {
-    document.getElementById("navbar").style.background = "white";
-    document.getElementById("navbar").style.padding = "30px 50px";
-    document.getElementById("navbar").style.boxShadow = "0 0 5px #999999";
-
-  }
-  else {
-    document.getElementById("navbar").style.background = "transparent";
-    document.getElementById("navbar").style.padding = "50px 10px";
-    document.getElementById("navbar").style.boxShadow = "0 0 0px";
-  }
-
-
-}
-
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('header');
+  let windowPosition = window.scrollY > 480;
+  header.classList.toggle('scrolling-active', windowPosition);
+})
