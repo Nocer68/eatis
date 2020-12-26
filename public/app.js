e4c8c684f907a5fa73dd3d4a0de088097fc37708
Valentin Figueiredo <valentinfigueiredo@protonmail.com>
2020-12-26 01:31:25 +0100

    navbarok

--- public/app.js
+++ public/app.js
@@ -3,3 +3,12 @@ window.addEventListener('scroll', function () {
   let windowPosition = window.scrollY > 480;
   header.classList.toggle('scrolling-active', windowPosition);
 })
+
+$(document).ready(function() {
+  $('.nav-toggler').each(function(_, navToggler) {
+    $(navToggler).on('click', function() {
+      var target = $(navToggler).data('menu');
+      $(target).toggleClass('hidden');
+    });
+  });
+});
